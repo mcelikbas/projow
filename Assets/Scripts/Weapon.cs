edb5e6fa6f82d537cbdd59a075a9fc4fa7069cf6
alexis <alexis.monterrat@gmail.com>
2019-01-24 21:17:14 +0100

    animation shoot and spear

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,18 +7,27 @@ public class Weapon : MonoBehaviour
     public Transform firePoint;
     public GameObject bulletPrefab;
 
-  
+    private Animator animator;
 
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     void Update()
     {
         if(Input.GetButtonDown("Fire1"))
         {
+           
             Shoot();
         }
+       
+
     }
 
     void Shoot ()
     {
+        animator.SetTrigger("shoot");
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
     }
 }
